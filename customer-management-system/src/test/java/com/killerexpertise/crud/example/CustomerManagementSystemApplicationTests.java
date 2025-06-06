6338fb875775ce06e9b2a5a3b9fca1e729f43832
Vishal Chothe <163386224+thevishalchothe@users.noreply.github.com>
2025-06-06 22:40:49 +0530

    Initial commit: Created Customer Management System project

---
+++ customer-management-system/src/test/java/com/killerexpertise/crud/example/CustomerManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.killerexpertise.crud.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
