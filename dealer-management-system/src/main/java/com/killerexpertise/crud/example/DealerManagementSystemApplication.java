c8d1777ea9a71cc0e43bca1f3d7f876b8aa43648
vishal-chothe-patil <vishalchothe134@gmail.com>
2025-06-10 12:55:52 +0530

    Initial commit: Created dealer-management-system project and structure

---
+++ dealer-management-system/src/main/java/com/killerexpertise/crud/example/DealerManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.killerexpertise.crud.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DealerManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DealerManagementSystemApplication.class, args);
+	}
+
+}
---
+++ dealer-management-system/src/test/java/com/killerexpertise/crud/example/dealer_management_system/DealerManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.killerexpertise.crud.example.dealer_management_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DealerManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
