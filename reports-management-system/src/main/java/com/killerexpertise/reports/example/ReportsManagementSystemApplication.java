77bfd256fc987c161fd054f988274f2797a97e8c
vishal-chothe-patil <vishalchothe134@gmail.com>
2025-06-11 22:42:39 +0530

    Initial commit: project created and add properties

---
+++ reports-management-system/src/main/java/com/killerexpertise/reports/example/ReportsManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.killerexpertise.reports.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReportsManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReportsManagementSystemApplication.class, args);
+	}
+
+}
---
+++ reports-management-system/src/test/java/com/killerexpertise/reports/example/ReportsManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.killerexpertise.reports.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReportsManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
